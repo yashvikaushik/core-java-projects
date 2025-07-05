6df2747e15d6d6f8f8a5bd1015b9a17df0757592
yashvikaushik <yashvikaushik2525@gmail.com>
2025-07-05 16:08:03 +0530

    Student Management System Service class

---
+++ StudentManagementSystem/src/StudentManager.java
@@ -0,0 +1,44 @@
+import java.io.File;
+
+public class StudentManager {
+
+    StudentFileHandler studentFileHandler=new StudentFileHandler();
+    
+
+    public boolean addStudent(Student student){
+        if(studentFileHandler.saveStudent(student))
+        return true;
+        else
+        return false;
+    }
+
+    public boolean deleteStudent(int id){
+       if(studentFileHandler.deleteStudent(id))
+       return true;
+       else
+       return false;
+    }
+
+    public Student getStudent(int id){
+       Student obj= studentFileHandler.getStudent(id);
+       return obj;
+
+    }
+
+    public Student[] getAllStudent(){
+        return studentFileHandler.loadStudents();
+        //return student;
+        //System.out.println(studentFileHandler.loadStudents());
+    }
+
+    public int getStudentIndex(int id){
+       int index= studentFileHandler.getStudentIndex(id);
+       return index;
+    }
+
+   
+
+    
+           
+    
+}
