6c15a8d5325d649dcb29aaaf12b7452dc629cf95
yashvikaushik <yashvikaushik2525@gmail.com>
2025-07-22 19:31:54 +0530

    user model

---
+++ CineMate/src/movieTicketsBookingSystem/model/User.java
@@ -0,0 +1,69 @@
+package movieTicketsBookingSystem.model;
+
+public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private String role;
+
+    public User(){
+
+    }
+
+    //constructor
+    public User(int id,String userName,String email,String password){
+        this.id=id;
+        this.userName=userName;
+        this.email=email;
+        this.password=password;
+        this.role=role;
+}
+
+//getters
+
+public int getID(){
+    return id;
+}
+
+public String getUserName(){
+return userName;
+}
+
+public String getEmail(){
+return email;
+}
+
+public String getPassword(){
+return password;
+}
+
+public String getRole(){
+return role;
+}
+
+//setters
+
+public void setID(int id){
+    this.id=id;
+}
+
+public void setUserName(String userName){
+this.userName=userName;
+}
+
+public void setEmail(String email){
+this.email=email;
+}
+
+public void setPassword(String password){
+this.password=password;
+}
+
+public void setRole(String role){
+this.role=role;
+}
+
+
+    
+}
