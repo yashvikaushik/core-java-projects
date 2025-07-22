1abd1a195fafe16d6aab111d230375ddc00f687c
yashvikaushik <yashvikaushik2525@gmail.com>
2025-07-22 19:31:34 +0530

    connection

---
+++ CineMate/src/movieTicketsBookingSystem/dal/DBConnection.java
@@ -0,0 +1,25 @@
+package movieTicketsBookingSystem.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/cineMate";
+    private static final String USER = "root2";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Connection failed. Check database settings.");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
