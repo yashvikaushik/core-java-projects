df17b2f24f883660f070a31d37c2085afd0f407e
yashvikaushik <yashvikaushik2525@gmail.com>
2025-07-25 22:27:00 +0530

    updated Movie pojo

--- CineMate/src/movieTicketsBookingSystem/model/Movie.java
+++ CineMate/src/movieTicketsBookingSystem/model/Movie.java
@@ -3,11 +3,13 @@ package movieTicketsBookingSystem.model;
 import javax.swing.ImageIcon;
 
 public class Movie {
+    int id;
     private String title;
     private String description;
     private ImageIcon poster;
 
-    public Movie(String title, String description, ImageIcon poster) {
+    public Movie(int id, String title, String description, ImageIcon poster) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.poster = poster;
@@ -23,6 +25,10 @@ public class Movie {
 
     public ImageIcon getPoster() {
         return poster;
+    }
+
+    public int getID() {
+        return id;
     }
 
     
